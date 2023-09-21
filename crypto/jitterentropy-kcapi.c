1d77f597bed7ad9040cec3613dc7cfedb513b527
Stephan Müller <smueller@chronox.de>
2023-09-21 13:48:33 +0200

    crypto: jitter - Allow configuration of memory size
    The memory size consumed by the Jitter RNG is one contributing factor in
    the amount of entropy that is gathered. As the amount of entropy
    directly correlates with the distance of the memory from the CPU, the
    caches that are possibly present on a given system have an impact on the
    collected entropy.
    
    Thus, the kernel compile time should offer a means to configure the
    amount of memory used by the Jitter RNG. Although this option could be
    turned into a runtime option (e.g. a kernel command line option), it
    should remain a compile time option as otherwise adminsitrators who may
    not have performed an entropy assessment may select a value that is
    inappropriate.
    
    The default value selected by the configuration is identical to the
    current Jitter RNG value. Thus, the patch should not lead to any change
    in the Jitter RNG behavior.
    
    To accommodate larger memory buffers, kvzalloc / kvfree is used.
    
    Signed-off-by: Stephan Mueller <smueller@chronox.de>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/jitterentropy-kcapi.c
+++ crypto/jitterentropy-kcapi.c
@@ -55,6 +55,17 @@
  * Helper function
  ***************************************************************************/
 
+void *jent_kvzalloc(unsigned int len)
+{
+	return kvzalloc(len, GFP_KERNEL);
+}
+
+void jent_kvzfree(void *ptr, unsigned int len)
+{
+	memzero_explicit(ptr, len);
+	kvfree(ptr);
+}
+
 void *jent_zalloc(unsigned int len)
 {
 	return kzalloc(len, GFP_KERNEL);
