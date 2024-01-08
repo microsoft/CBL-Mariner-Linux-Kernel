c569fd99b53c40111f60734a7190881e08bb1617
Fan Wu <wufan@linux.microsoft.com>
2024-01-08 11:17:39 -0800

    fs|security: add security_file_set_userspace_pathname security hook
    The new security hook provides a way for LSMs to access the
    filename structure passed into an path_openat call, without
    accessing the nameidata structure provided by current.
    
    This is useful for LSMs that wish to use the application-
    provided userland path as a representation for the application's
    intent to load a specific file, e.g. config files in /etc.
    
    Link: https://microsoft.visualstudio.com/OS/_workitems/edit/23803553
    TAG_MSFT: overlake
    
    Signed-off-by: Fan Wu <wufan@linux.microsoft.com>

--- include/linux/lsm_hook_defs.h
+++ include/linux/lsm_hook_defs.h
@@ -189,6 +189,8 @@ LSM_HOOK(int, 0, file_send_sigiotask, struct task_struct *tsk,
 LSM_HOOK(int, 0, file_receive, struct file *file)
 LSM_HOOK(int, 0, file_open, struct file *file)
 LSM_HOOK(int, 0, file_truncate, struct file *file)
+LSM_HOOK(int, 0, file_set_userspace_pathname, struct file *file,
+	 const struct filename *name)
 LSM_HOOK(int, 0, task_alloc, struct task_struct *task,
 	 unsigned long clone_flags)
 LSM_HOOK(void, LSM_RET_VOID, task_free, struct task_struct *task)
