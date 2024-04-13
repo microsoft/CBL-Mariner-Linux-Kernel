e503dea24021086a77727b836308e735967803dc
Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
2024-04-13 00:59:35 -0500

    virt: Introduce Hypervisor Enforced Kernel Integrity (Heki)
    Hypervisor Enforced Kernel Integrity (Heki) is a feature that will use
    the hypervisor to enhance guest virtual machine security. Depending on
    the type of hypervisor (Type 1 vs Type 2) the feature can be extended to
    host partition as well.
    
    Implement minimal code to introduce Heki and lock critical registers
    down as the first set of guest protections. Changes involve:
    
    - Define the config option for Heki
    
    - Define a kernel command line parameter "heki" to turn the feature on
      or off. By default, Heki is on.
    
    - Define a heki_hypervisor structure to house the API functions. A
      hypervisor that supports Heki must instantiate a heki_hypervisor
    structure and pass it to the Heki common code. This allows the common
    code to access these functions in a hypervisor-agnostic way.
    
    - Introduce lock_crs() in heki_hypervisor that can be plugged in by
      supporting hypervisors to hypervisoe backed enable pinning down of
    critical system regiters.
    
    - Introduce a late init hook for heki that is called that the end of
      kernel boot before the first user space process is run. This hook in
    turn calls into hypervisor supported hooks to enforce various
    protections. Call into hyperviso lock_crs hook as the first step.  init.
    
    Co-developed-by: Mickaël Salaün <mic@digikod.net>
    Signed-off-by: Mickaël Salaün <mic@digikod.net>
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
    Signed-off-by: Thara Gopinath <tgopinath@microsoft.com>

--- init/main.c
+++ init/main.c
@@ -100,6 +100,7 @@
 #include <linux/init_syscalls.h>
 #include <linux/stackdepot.h>
 #include <linux/randomize_kstack.h>
+#include <linux/heki.h>
 #include <net/net_namespace.h>
 
 #include <asm/io.h>
@@ -1451,6 +1452,7 @@ static int __ref kernel_init(void *unused)
 	exit_boot_config();
 	free_initmem();
 	mark_readonly();
+	heki_late_init();
 
 	/*
 	 * Kernel mappings are now finalized - update the userspace page-table
