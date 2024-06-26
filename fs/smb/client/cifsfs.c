e448d44f25af5656ba4a9a3edc2d545cbaa98e8b
Steve French <stfrench@microsoft.com>
2024-06-26 14:42:59 -0500

    cifs: fix HMAC MD5 availability
    HMAC(MD5) is needed for some important default code paths in cifs,
    including for a part of non-Kerberos authentication (and for recognizing
    and also for creating mfsymlinks), but there are cases where the md5
    driver is not available. Since that code is so small, compile it into
    cifs.ko, to reduce the risk of unexpected errors.
    
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>

--- fs/smb/client/cifsfs.c
+++ fs/smb/client/cifsfs.c
@@ -1968,6 +1968,7 @@ init_cifs(void)
 		goto out_init_cifs_idmap;
 	}
 
+	smb3_crypto_register();
 	return 0;
 
 out_init_cifs_idmap:
@@ -2036,6 +2037,7 @@ exit_cifs(void)
 	destroy_workqueue(serverclose_wq);
 	destroy_workqueue(cfid_put_wq);
 	destroy_workqueue(cifsiod_wq);
+	smb3_crypto_unregister();
 	cifs_proc_clean();
 }
 
