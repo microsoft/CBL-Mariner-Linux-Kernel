2f8df66109f94bcdbe3753cc9159e570977aadf0
Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
2024-07-10 01:08:16 -0500

    Pass a VTL0 module to VTL1 after post relocation fixups
    Move the call to validate a VTL0 module after post relocation fixes
    to just before complete_formation(). In VTL1, the same post relocation
    fixes will be applied independently and module contents will be
    validated.
    
    After that, VTL1 will set proper EPT permissions for the VTL0 module
    sections.
    
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>

--- virt/heki/module.c
+++ virt/heki/module.c
@@ -85,6 +85,8 @@ void heki_load_kdata(void)
 	heki_kinfo.ksymtab_gpl_end =
 			(struct kernel_symbol *)__stop___ksymtab_gpl;
 
+	heki_load_arch_kinfo(&heki_kinfo);
+
 	args.attributes = HEKI_KERNEL_INFO;
 	heki_walk((unsigned long)&heki_kinfo,
 		  (unsigned long)&heki_kinfo + sizeof(heki_kinfo),
