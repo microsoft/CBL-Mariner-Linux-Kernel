e6ed63174445ae64a5a5ef8371fc425b473d7bca
Allen Pais <apais@linux.microsoft.com>
2024-07-29 17:35:10 +0000

    efi: Lock down the kernel if booted in secure boot mode
    Based on an earlier patch by David Howells, who wrote the following
    description:
    
    > UEFI Secure Boot provides a mechanism for ensuring that the firmware will
    > only load signed bootloaders and kernels.  Certain use cases may also
    > require that all kernel modules also be signed.  Add a configuration option
    > that to lock down the kernel - which includes requiring validly signed
    > modules - if the kernel is secure-booted.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    [Salvatore Bonaccorso: After fixing https://bugs.debian.org/956197 the
    help text for LOCK_DOWN_IN_EFI_SECURE_BOOT was adjusted to mention that
    lockdown is triggered in integrity mode (https://bugs.debian.org/1025417)]
    Signed-off-by: Salvatore Bonaccorso <carnil@debian.org>
    
    Backported to v6.6.40 - AzLinux
    
    Signed-off-by: Allen Pais <apais@linux.microsoft.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1028,6 +1028,8 @@ void __init setup_arch(char **cmdline_p)
 	if (efi_enabled(EFI_BOOT))
 		efi_init();
 
+	efi_set_secure_boot(boot_params.secure_boot);
+
 	reserve_ibft_region();
 	x86_init.resources.dmi_setup();
 
@@ -1189,8 +1191,6 @@ void __init setup_arch(char **cmdline_p)
 #endif
 	/* Allocate bigger log buffer */
 	setup_log_buf(1);
-
-	efi_set_secure_boot(boot_params.secure_boot);
 
 	reserve_initrd();
 
