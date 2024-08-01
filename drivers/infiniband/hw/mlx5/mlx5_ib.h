f6e6805b6db5d2d6fc50ff4f559d57b7d555c406
Yishai Hadas <yishaih@nvidia.com>
2024-08-01 15:05:12 +0300

    RDMA/mlx5: Add the initialization flow to utilize the 'data direct' device
    Add the NET device initialization flow to utilize the 'data
    direct' device.
    
    When a NET mlx5_ib device is capable of 'data direct', the following
    sequence of actions will occur:
    - Find its affiliated 'data direct' VUID via a firmware command.
    - Create its own private PD and 'data direct' mkey.
    - Register to be notified when its 'data direct' driver is probed or removed.
    
    The DMA device of the affiliated 'data direct' device, including the
    private PD and the 'data direct' mkey, will be used later during MR
    registrations that request the data direct functionality.
    
    Signed-off-by: Yishai Hadas <yishaih@nvidia.com>
    Link: https://patch.msgid.link/b11fa87b2a65bce4db8d40341bb6cee490fa4d06.1722512548.git.leon@kernel.org
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    (cherry picked from commit 2e8e631d7a41e3a4edc94f3c9dd5cb32c2aa539e)
    Signed-off-by: Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>

--- drivers/infiniband/hw/mlx5/mlx5_ib.h
+++ drivers/infiniband/hw/mlx5/mlx5_ib.h
@@ -818,6 +818,11 @@ struct mlx5_ib_port_resources {
 	struct work_struct pkey_change_work;
 };
 
+struct mlx5_data_direct_resources {
+	u32 pdn;
+	u32 mkey;
+};
+
 struct mlx5_ib_resources {
 	struct ib_cq	*c0;
 	struct mutex cq_lock;
@@ -1173,6 +1178,7 @@ struct mlx5_ib_dev {
 	u16 pkey_table_len;
 	u8 lag_ports;
 	struct mlx5_special_mkeys mkeys;
+	struct mlx5_data_direct_resources ddr;
 
 #ifdef CONFIG_MLX5_MACSEC
 	struct mlx5_macsec macsec;
