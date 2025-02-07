34f46a2081dac35bbcd370c8f8f86e57e4b86077
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2025-02-07 11:03:22 -0800

    hyperv: Move arch/x86/hyperv/hv_proc.c to drivers/hv
    These helpers are not specific to x86_64 and will be needed by common code.
    Remove some unnecessary #includes.
    
    Reviewed-by: Michael Kelley <mhklinux@outlook.com>
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>
    Link: https://lore.kernel.org/r/1738955002-20821-3-git-send-email-nunodasneves@linux.microsoft.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>
    Message-ID: <1738955002-20821-3-git-send-email-nunodasneves@linux.microsoft.com>
    (cherry picked from commit 0222eb30a3572cc9c4e2f0a3bb37f8f71089f2b6)

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -57,10 +57,6 @@ u64 hv_tdx_hypercall(u64 control, u64 param1, u64 param2);
 #define HV_AP_INIT_GPAT_DEFAULT		0x0007040600070406ULL
 #define HV_AP_SEGMENT_LIMIT		0xffffffff
 
-int hv_call_add_logical_proc(int node, u32 lp_index, u32 acpi_id);
-int hv_call_notify_all_processors_started(void);
-bool hv_lp_exists(u32 lp_index);
-
 /*
  * If the hypercall involves no input or output parameters, the hypervisor
  * ignores the corresponding GPA pointer.
