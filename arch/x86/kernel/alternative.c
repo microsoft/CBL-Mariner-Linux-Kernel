f0709959a9374a160130c3dfa1414db34458d09e
Angelina Vu <angelinavu@microsoft.com>
2025-02-25 19:53:31 +0000

    Merged PR 191: VTL1: Enable CONFIG_RETPOLINE
    Use VTL0 __x86_indirect_thunk_array address to get register needed when patching retpoline for validating VTL0 modules.
    
    Signed-off-by: Angelina Vu <angelinavu@microsoft.com>
    
    ----
    New feature
    
    This pull request enables the `CONFIG_RETPOLINE` configuration to enhance security against speculative execution attacks.
    - `arch/x86/kernel/alternative.c`: Added support for `heki_kinfo` to determine the address of the indirect thunk array.
    - `arch/x86/kernel/alternative.c`: Included the `linux/heki.h` header file.

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -20,6 +20,7 @@
 #include <linux/sync_core.h>
 #include <linux/moduleloader.h>
 #include <linux/cleanup.h>
+#include <linux/heki.h>
 #include <asm/text-patching.h>
 #include <asm/alternative.h>
 #include <asm/sections.h>
@@ -761,9 +762,13 @@ static int patch_retpoline(void *addr, struct insn *insn, u8 *bytes)
 	retpoline_thunk_t *target;
 	int reg, ret, i = 0;
 	u8 op, cc;
+	struct heki_kinfo *kinfo = current->kinfo;
 
 	target = addr + insn->length + insn->immediate.value;
-	reg = target - __x86_indirect_thunk_array;
+	if (kinfo)
+		reg = target - kinfo->arch.indirect_thunk_array_addr;
+	else
+		reg = target - __x86_indirect_thunk_array;
 
 	if (WARN_ON_ONCE(reg & ~0xf))
 		return -1;
