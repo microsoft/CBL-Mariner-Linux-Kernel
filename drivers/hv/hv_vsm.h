b66de375e8ce84e4cd8a46effbe9df108665a0d9
Thara Gopinath <tgopinath@microsoft.com>
2025-02-25 19:53:15 +0000

    drivers: hv: hv_vsm_common: Add support to set and retrieve VTL0 registers from VTL1.
    VTL1 being higher privileged can read and write to VTL0 registers. Add
    apis to enable this. To aid this, split the exitising
    vsm_get/set_register to take input vtl as a parameter
    
    Signed-off-by: Thara Gopinath <tgopinath@microsoft.com>

--- drivers/hv/hv_vsm.h
+++ drivers/hv/hv_vsm.h
@@ -39,6 +39,10 @@ union hv_register_vsm_code_page_offsets {
 
 int hv_vsm_boot_init(void);
 
+
+int __hv_vsm_get_register(u32 reg_name, u64 *result, u8 input_vtl);
+int __hv_vsm_set_register(u32 reg_name, u64 value, u8 input_vtl);
+
 int hv_vsm_get_register(u32 reg_name, u64 *result);
 int hv_vsm_set_register(u32 reg_name, u64 value);
 int hv_vsm_get_code_page_offsets(void);
