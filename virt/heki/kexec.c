149238a49d3eceb2fe8a029011bbc6a5f4a5a073
Madhavan T. Venkataraman <madvenka@microsoft.com>
2025-02-28 12:51:18 -0600

    VTL0: Send all of the kexec data to VTL1
    At the end of the kexec_file_load() syscall, we need to send all of the
    data associated with the kexec kernel image to VTL1 so the data can
    be validated and write-protected.
    
    So far, only the kimage structure and the kernel blob have been sent.
    Send the rest of the data.
    
    	See bzImage64_load() for how the kexec segments are allocated and
    	their contents prepared. The contents are prepared in temporary
    	buffers.
    
    	See kimage_load_segment() for how the temporary buffers are copied
    	to their permanent pages. These permanent pages need to be write
    	protected until the actual kexec.
    
    	Pages common to crash kexec and normal kexec
    	--------------------------------------------
    
    	- Kernel image		(user passed)
    	- Boot Parameters	(user passed cmdline included in this)
    	- Initrd		(user passed)
    	- Purgatory
    	- Page table pages
    
    	Crash kexec specific pages
    	--------------------------
    
    	- ELF headers
    
    	Normal kexec specific pages
    	---------------------------
    
    	- Indirection pages
    
    See the comments above the following functions:
    
    	- heki_load_segments()
    	- heki_load_entries()
    	- heki_load_control_pages()
    	- heki_load_arch_pages()
    
    - VTL1 will make the above pages read-only after signature verification.
    
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>

--- virt/heki/kexec.c
+++ virt/heki/kexec.c
@@ -24,6 +24,105 @@ void heki_copy_kernel(void *kernel, unsigned long kernel_len)
 	memcpy(heki_kernel, kernel, kernel_len);
 }
 
+void heki_load_pages(unsigned long pfn, struct heki_args *args)
+{
+	struct page *page = boot_pfn_to_page(pfn);
+	unsigned int order = page_private(page);
+	size_t size = 1UL << (order + PAGE_SHIFT);
+	unsigned long pa = pfn << PAGE_SHIFT;
+	void *va = __va(pa);
+
+	args->attributes = HEKI_KEXEC_PAGES;
+	heki_walk((unsigned long) va, (unsigned long) va + size,
+		  heki_get_ranges, args);
+}
+
+/*
+ * For a normal kexec, memory is only ear-marked for kexec segments and not
+ * actually allocated. The current kernel could be using that ear-marked
+ * memory. So, segment contents are first copied to separate pages called
+ * source pages. Just before jumping to the new kernel, the source pages are
+ * copied to their corresponding destination pages in the ear-marked memory.
+ *
+ * The source and the destination page addresses are recorded in the image as
+ * an array of entries in an entry page. If there are more entries than will
+ * fit into an entry page, the entries are recorded in multiple entry pages and
+ * the entry pages are chained together using indirection entries. Basically,
+ * it is a linked list.
+ *
+ * Walk the list and load the source pages. Load the indirection pages as well.
+ * These will be write-protected until the source pages are copied into the
+ * destination pages just before jumping to the new kernel.
+ */
+void heki_load_entries(struct kimage *image, struct heki_args *args)
+{
+	kimage_entry_t *ptr, entry;
+	kimage_entry_t ind = 0;
+
+	for_each_kimage_entry(image, ptr, entry) {
+		if (entry & IND_INDIRECTION) {
+			/* Load the previous indirection page */
+			if (ind & IND_INDIRECTION)
+				heki_load_pages(ind >> PAGE_SHIFT, args);
+			/* Save this indirection page. */
+			ind = entry;
+		} else if (entry & IND_SOURCE)
+			heki_load_pages(entry >> PAGE_SHIFT, args);
+	}
+	/* Load the final indirection page */
+	if (ind & IND_INDIRECTION)
+		heki_load_pages(ind >> PAGE_SHIFT, args);
+}
+
+/*
+ * Load control pages setup for kexec (code page, page table pages, etc)
+ * so they can be write-protected until we jump to the new kernel.
+ */
+void heki_load_control_pages(struct kimage *image, struct heki_args *args)
+{
+	struct list_head *list = &image->control_pages;
+	struct page *page;
+
+	list_for_each_entry(page, list, lru) {
+		if (page == image->vmcoreinfo_page) {
+			/*
+			 * This page is modified before jumping to the new
+			 * kernel. It cannot be made read-only in the EPT at
+			 * file load time. It doesn't need to be. So, don't
+			 * load it.
+			 */
+			continue;
+		}
+		heki_load_pages(page_to_pfn(page), args);
+	}
+}
+
+/*
+ * For crash kexec, segment memory is reserved up front. So, segment contents
+ * are copied to reserved memory at kexec file load time. Load the segment
+ * pages so they can be write-protected until we jump to the new kernel.
+ */
+static void heki_load_segment(struct kimage *image, int seg,
+			      struct heki_args *args)
+{
+	struct kexec_segment *segment = &image->segment[seg];
+	size_t size = segment->memsz;
+	unsigned long pa = segment->mem;
+	void *va = __va(pa);
+
+	args->attributes = HEKI_KEXEC_PAGES;
+	heki_walk((unsigned long) va, (unsigned long) va + size,
+		  heki_get_ranges, args);
+}
+
+void heki_load_segments(struct kimage *image, struct heki_args *args)
+{
+	int i;
+
+	for (i = 0; i < image->nr_segments; i++)
+		heki_load_segment(image, i, args);
+}
+
 int heki_kexec_validate(struct kimage *image)
 {
 	struct heki_hypervisor *hypervisor = heki.hypervisor;
@@ -45,6 +144,13 @@ int heki_kexec_validate(struct kimage *image)
 	heki_walk((unsigned long) heki_kernel,
 		  (unsigned long) heki_kernel + heki_kernel_len,
 		  heki_get_ranges, &args);
+
+	if (crash)
+		heki_load_segments(image, &args);
+	else
+		heki_load_entries(image, &args);
+	heki_load_control_pages(image, &args);
+	heki_load_arch_pages(image, &args);
 
 	ret = hypervisor->kexec_validate(args.head_pa, args.nranges, crash);
 	if (ret)
