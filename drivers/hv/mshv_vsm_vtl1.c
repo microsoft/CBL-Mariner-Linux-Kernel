heki-linux/lvbs-linux

Free module init sections hcall
Implement a hypercall to set default permissions (RW) for the init
sections of a VTL0 module. These need to be freed after module init.
So, their EPT permissions must be restored to RW.

Along with this, also set the EPT permissions of the ro_after_init
data section of a module to R__ (read-only).

Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>

drivers/hv/mshv_vsm_vtl1.c
@@ -124,6 +124,7 @@ enum vsm_service_ids {
 	VSM_VTL_CALL_FUNC_ID_PROTECT_MEMORY = 0x1FFE4,
 	VSM_VTL_CALL_FUNC_ID_LOAD_KDATA = 0x1FFE5,
 	VSM_VTL_CALL_FUNC_ID_VALIDATE_MODULE = 0x1FFE6,
+	VSM_VTL_CALL_FUNC_ID_FREE_MODULE_INIT = 0x1FFE7,
 };
 
 struct hv_vsm_per_cpu {
@@ -1159,6 +1160,58 @@ static int vsm_set_guest_module_permissions(struct heki_mod *hmod)
 	return err;
 }
 
+static int vsm_free_guest_module_init(long token)
+{
+	struct heki_mod *hmod;
+	bool found;
+	unsigned long permissions;
+	int err = 0;
+
+	mutex_lock(&vtl0.lock);
+
+	found = false;
+	list_for_each_entry(hmod, &vtl0.modules, node) {
+		if (hmod->token == token) {
+			found = true;
+			break;
+		}
+	}
+
+	if (!found) {
+		/* Silently ignore the request. */
+		goto unlock;
+	}
+
+	for_each_mod_mem_type(type) {
+		bool free = true;
+
+		switch (type) {
+		case MOD_RO_AFTER_INIT:
+			permissions = MEM_ATTR_READ;
+			free = false;
+			break;
+		case MOD_INIT_TEXT:
+			permissions = MEM_ATTR_READ | MEM_ATTR_WRITE;
+			break;
+		case MOD_INIT_DATA:
+			permissions = MEM_ATTR_READ | MEM_ATTR_WRITE;
+			break;
+		case MOD_INIT_RODATA:
+			permissions = MEM_ATTR_READ | MEM_ATTR_WRITE;
+			break;
+		default:
+			continue;
+		}
+
+		err = vsm_set_module_permissions(hmod, type, permissions, free);
+		if (err)
+			break;
+	}
+unlock:
+	mutex_unlock(&vtl0.lock);
+	return err;
+}
+
 static void vsm_resolve_func(char *name, Elf64_Sym *sym);
 
 static long mshv_vsm_validate_guest_module(u64 pa, unsigned long nranges,
@@ -1687,6 +1740,10 @@ static void mshv_vsm_handle_entry(struct mshv_vtl_call_params *_vtl_params)
 		status = mshv_vsm_validate_guest_module(_vtl_params->_a1,
 							_vtl_params->_a2,
 							_vtl_params->_a3);
+		break;
+	case VSM_VTL_CALL_FUNC_ID_FREE_MODULE_INIT:
+		pr_debug("%s : VSM_FREE_MODULE_INIT\n", __func__);
+		status = vsm_free_guest_module_init(_vtl_params->_a1);
 		break;
 	default:
 		pr_err("%s: Wrong Command:0x%llx sent into VTL1\n", __func__, _vtl_params->_a0);

microsoft/CBL-Mariner-Linux-Kernel

drivers/hv/mshv_vsm_vtl1.c
@@ -1236,6 +1236,58 @@ static int vsm_set_guest_module_permissions(struct heki_mod *hmod)
 	return err;
 }
 
+static int vsm_free_guest_module_init(long token)
+{
+	struct heki_mod *hmod;
+	bool found;
+	unsigned long permissions;
+	int err = 0;
+
+	mutex_lock(&vtl0.lock);
+
+	found = false;
+	list_for_each_entry(hmod, &vtl0.modules, node) {
+		if (hmod->token == token) {
+			found = true;
+			break;
+		}
+	}
+
+	if (!found) {
+		/* Silently ignore the request. */
+		goto unlock;
+	}
+
+	for_each_mod_mem_type(type) {
+		bool free = true;
+
+		switch (type) {
+		case MOD_RO_AFTER_INIT:
+			permissions = MEM_ATTR_READ;
+			free = false;
+			break;
+		case MOD_INIT_TEXT:
+			permissions = MEM_ATTR_READ | MEM_ATTR_WRITE;
+			break;
+		case MOD_INIT_DATA:
+			permissions = MEM_ATTR_READ | MEM_ATTR_WRITE;
+			break;
+		case MOD_INIT_RODATA:
+			permissions = MEM_ATTR_READ | MEM_ATTR_WRITE;
+			break;
+		default:
+			continue;
+		}
+
+		err = vsm_set_module_permissions(hmod, type, permissions, free);
+		if (err)
+			break;
+	}
+unlock:
+	mutex_unlock(&vtl0.lock);
+	return err;
+}
+
 static void vsm_resolve_func(char *name, Elf64_Sym *sym);
 
 static long mshv_vsm_validate_guest_module(u64 pa, unsigned long nranges,
@@ -1685,6 +1737,10 @@ static void mshv_vsm_handle_entry(struct hv_vtlcall_param *_vtl_params)
 		status = mshv_vsm_validate_guest_module(_vtl_params->a1,
 							_vtl_params->a2,
 							_vtl_params->a3);
+		break;
+	case VSM_VTL_CALL_FUNC_ID_FREE_MODULE_INIT:
+		pr_debug("%s : VSM_FREE_MODULE_INIT\n", __func__);
+		status = vsm_free_guest_module_init(_vtl_params->a1);
 		break;
 	default:
 		pr_err("%s: Wrong Command:0x%llx sent into VTL1\n", __func__, _vtl_params->a0);