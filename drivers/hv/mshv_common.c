efa736ddbc81069e8b1fec05ca202004a869ab70
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2025-03-25 16:43:40 -0700

    mshv: backporting: fix uninitialized status in hv_call_set_vp_registers
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

--- drivers/hv/mshv_common.c
+++ drivers/hv/mshv_common.c
@@ -85,7 +85,7 @@ int hv_call_set_vp_registers(
 	u16 completed = 0;
 	unsigned long remaining = count;
 	int rep_count;
-	u64 status;
+	u64 status = HV_STATUS_SUCCESS;
 	unsigned long flags;
 
 	local_irq_save(flags);
