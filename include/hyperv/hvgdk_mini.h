690e0fe5b79ff1196ccf0c7dcffe77371e9563bb
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2025-03-28 10:27:19 -0700

    hyperv: backporting: s/hv_status_to/hv_result_to/g
    Renamed in upstream
    
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

--- include/hyperv/hvgdk_mini.h
+++ include/hyperv/hvgdk_mini.h
@@ -16,7 +16,7 @@ struct hv_u128 {
 	u64 high_part;
 } __packed;
 
-/* NOTE: when adding below, update hv_status_to_string() */
+/* NOTE: when adding below, update hv_result_to_string() */
 #define HV_STATUS_SUCCESS			    0x0
 #define HV_STATUS_INVALID_HYPERCALL_CODE	    0x2
 #define HV_STATUS_INVALID_HYPERCALL_INPUT	    0x3
