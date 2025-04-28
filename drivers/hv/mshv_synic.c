ce0d3cc3b137107a1e72e3bd7240310905742b83
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2025-04-28 14:56:08 -0700

    Drivers: hv: Fix deref of partition before checking its validity
    The partition pointer should be checked before dereferencing it, not
    after.
    
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

--- drivers/hv/mshv_synic.c
+++ drivers/hv/mshv_synic.c
@@ -151,7 +151,6 @@ static bool mshv_async_call_completion_isr(struct hv_message *msg)
 	rcu_read_lock();
 
 	partition = mshv_partition_find(partition_id);
-	partition->async_hypercall_status = async_msg->status;
 
 	if (unlikely(!partition)) {
 		pr_err("%s: failed to find partition %llu\n",
@@ -162,6 +161,7 @@ static bool mshv_async_call_completion_isr(struct hv_message *msg)
 	pr_debug("%s: Partition ID: %llu completing async hypercall\n",
 		 __func__, async_msg->partition_id);
 
+	partition->async_hypercall_status = async_msg->status;
 	complete(&partition->async_hypercall);
 
 	handled = true;
