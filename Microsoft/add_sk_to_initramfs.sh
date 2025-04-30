c170f518926f8941e2949c5cc45cf5383288b3a7
Thara Gopinath <tgopinath@microsoft.com>
2025-04-30 07:18:50 -0400

    Microsoft: add_sk_to_initramfs.sh: Clean up script
    add_sk_to_initramfs.sh is used to add secure kernel to ubuntu initramfs.
    Clean up the script to remove adding and loading secure loader. Also change
    the script to load secure kernel in elf format rather than in binary
    format.
    
    Signed-off-by: Thara Gopinath <tgopinath@microsoft.com>

--- Microsoft/add_sk_to_initramfs.sh
+++ Microsoft/add_sk_to_initramfs.sh
@@ -6,45 +6,11 @@
 
 #usage: run sudo ./add_sk_to_initramfs.sh <path_to_optee> (i.e. ../optee-os)
 export OUT_DIR=$1
-export SKERNEL_LOAD_FILE="$1/skloader.bin"
-export SKERNEL_LOAD_FILE_ABSPATH="$(readlink -f $SKERNEL_LOAD_FILE)"
-export SKERNEL_FILE="$1/vmlinux.bin"
+export SKERNEL_FILE="$1/vmlinux"
 export SKERNEL_FILE_ABSPATH="$(readlink -f $SKERNEL_FILE)"
-export SKERNEL_LOAD_SIG_FILE="$1/skloader.bin.p7s"
-export SKERNEL_LOAD_SIG_FILE_ABSPATH="$(readlink -f $SKERNEL_LOAD_SIG_FILE)"
-export SKERNEL_SIG_FILE="$1/vmlinux.bin.p7s"
+export SKERNEL_SIG_FILE="$1/vmlinux.p7s"
 export SKERNEL_SIG_FILE_ABSPATH="$(readlink -f $SKERNEL_SIG_FILE)"
 export SKERNEL_HOOK_FILE="/usr/share/initramfs-tools/hooks/skernel"
-
-# If skloader.bin file does not exist, exit
-if [[ ! -f $SKERNEL_LOAD_FILE ]]
-then
-   echo "Error: $SKERNEL_LOAD_FILE_ABSPATH does not exist."
-   echo "Compile skloader.bin and then re-try."
-   exit 0
-fi
-
-# If skloader.bin file does not exist, exit
-if [[ ! -f $SKERNEL_FILE ]]
-then
-   echo "Error: $SKERNEL_FILE_ABSPATH does not exist."
-   echo "Compile secure linux kernel(vmlinux.bin) and then re-try."
-   exit 0
-fi
-
-# If skloader.bin.p7s file does not exist, print warning
-if [[ ! -f $SKERNEL_LOAD_SIG_FILE ]]
-then
-   echo "Warning: $SKERNEL_LOAD_SIG_FILE_ABSPATH does not exist."
-   echo "Set kernel config CONFIG_HYPERV_VSM_DISABLE_IMG_VERIFY. Otherwise, VTL1 will not boot."
-fi
-
-# If skloader.bin.p7s file does not exist, print warning
-if [[ ! -f $SKERNEL_SIG_FILE ]]
-then
-   echo "Warning: $SKERNEL_SIG_FILE_ABSPATH does not exist."
-   echo "Set kernel config CONFIG_HYPERV_VSM_DISABLE_IMG_VERIFY. Otherwise, VTL1 will not boot."
-fi
 
 # If hook script file does not already exist, create it and set its permissions
 if [[ ! -f $SKERNEL_HOOK_FILE ]]
@@ -73,15 +39,7 @@ esac
 . /usr/share/initramfs-tools/hook-functions
 # Begin real processing below this line
 
-cp $SKERNEL_LOAD_FILE_ABSPATH "\${DESTDIR}/lib/firmware"
 cp $SKERNEL_FILE_ABSPATH "\${DESTDIR}/lib/firmware"
-
-if [ -f $SKERNEL_LOAD_SIG_FILE_ABSPATH ]
-then
-    cp $SKERNEL_LOAD_SIG_FILE_ABSPATH "\${DESTDIR}/lib/firmware"
-else
-    echo "Warning: No signature file for Secure Loader is added in the initramfs."
-fi
 
 if [ -f $SKERNEL_SIG_FILE_ABSPATH ]
 then
