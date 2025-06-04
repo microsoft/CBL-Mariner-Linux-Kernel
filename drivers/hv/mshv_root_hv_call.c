a59d4aaaca03622fffc5bee35bfc6b1f16732695
Jinank Jain <jinankjain@microsoft.com>
2025-06-04 22:03:27 +0530

    Drivers: hv: Fix a typo in CONFIG_x86
    CONFIG_X86 was incorrectly typed as CONFIG_x86.
    
    Fixes: 3d3831118217 ("Drivers/hv: Adjust interrupt control structure for ARM64")
    Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

--- drivers/hv/mshv_root_hv_call.c
+++ drivers/hv/mshv_root_hv_call.c
@@ -492,7 +492,7 @@ int hv_call_assert_virtual_interrupt(
 	 * NOTE: dest_addr only needs to be provided while asserting an
 	 * interrupt on x86 platform
 	 */
-#if IS_ENABLED(CONFIG_x86)
+#if IS_ENABLED(CONFIG_X86)
 	input->dest_addr = dest_addr;
 #endif
 	input->control = control;
