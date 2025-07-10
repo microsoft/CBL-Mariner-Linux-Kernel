5fcd715f34725251619f618a0cf26cb8e7d79982
Anirudh Rayabharam (Microsoft) <anirudh@anirudhrb.com>
2025-07-10 12:32:42 +0000

    arm64: hyperv: detect old MSHV versions with SMCCC
    Older MSHV versions (such as AH2024 that is in the Azure fleet
    currently) return a different UUID. Look for that too when detecting
    if we're running on MSHV.
    
    Signed-off-by: Anirudh Rayabharam (Microsoft) <anirudh@anirudhrb.com>

--- arch/arm64/hyperv/mshyperv.c
+++ arch/arm64/hyperv/mshyperv.c
@@ -61,6 +61,21 @@ static bool __init hyperv_detect_via_smccc(void)
 	return arm_smccc_hypervisor_has_uuid(&hyperv_uuid);
 }
 
+static bool __init legacy_hyperv_detect_via_smccc(void)
+{
+	struct arm_smccc_res res = {};
+
+	if (arm_smccc_1_1_get_conduit() != SMCCC_CONDUIT_HVC)
+		return false;
+
+	arm_smccc_1_1_hvc(ARM_SMCCC_VENDOR_HYP_CALL_UID_FUNC_ID, &res);
+	if (res.a0 == SMCCC_RET_NOT_SUPPORTED)
+		return false;
+
+	return res.a0 == 0x7948734d && res.a1 == 0x56726570 && res.a2 == 0
+		&& res.a3 == 0;
+}
+
 static int __init hyperv_init(void)
 {
 	struct hv_get_vp_registers_output	result;
@@ -74,7 +89,8 @@ static int __init hyperv_init(void)
 	 *
 	 * In such cases, do nothing and return success.
 	 */
-	if (!hyperv_detect_via_acpi() && !hyperv_detect_via_smccc())
+	if (!hyperv_detect_via_acpi() && !hyperv_detect_via_smccc()
+		&& !legacy_hyperv_detect_via_smccc())
 		return 0;
 
 	/* Setup the guest ID */
