2a11f8da59f1158b5bc3cf02b57de77c94bad5dd
Muminul Islam <muislam@microsoft.com>
2025-09-25 00:42:09 -0700

    drivers: mshv: APIC_EOI is x86_64 specific
    Handling the HvMessage type HVMSG_X64_APIC_EOI
    needs to be conditional to x86_64 only
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- drivers/hv/mshv_synic.c
+++ drivers/hv/mshv_synic.c
@@ -343,6 +343,7 @@ mshv_intercept_isr(struct hv_message *msg)
 		goto unlock_out;
 	}
 
+#if defined(__x86_64__)
 	if (msg->header.message_type == HVMSG_X64_APIC_EOI) {
 		/*
 		 * Check if this gsi is registered in the
@@ -363,6 +364,7 @@ mshv_intercept_isr(struct hv_message *msg)
 			goto unlock_out;
 		}
 	}
+#endif
 
 	/*
 	 * We should get an opaque intercept message here for all intercept
