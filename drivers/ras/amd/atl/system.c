b888c3b9573983e30c19844fbaf84938dbb4bc7d
Yazen Ghannam <yazen.ghannam@amd.com>
2025-10-06 15:10:27 +0000

    RAS/AMD/ATL: Return error codes from helper functions
    Pass up error codes from helper functions rather than discarding them.
    
    Suggested-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Yazen Ghannam <yazen.ghannam@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    (cherry picked from commit 6553c68bc73dccfb3c8a9971ee40ed378adc0df4)

--- drivers/ras/amd/atl/system.c
+++ drivers/ras/amd/atl/system.c
@@ -296,10 +296,13 @@ static void dump_df_cfg(void)
 
 int get_df_system_info(void)
 {
-	if (determine_df_rev()) {
+	int ret;
+
+	ret = determine_df_rev();
+	if (ret) {
 		pr_warn("Failed to determine DF Revision");
 		df_cfg.rev = UNKNOWN;
-		return -EINVAL;
+		return ret;
 	}
 
 	if (df_cfg.flags.prm_only)
