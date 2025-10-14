f0f829af9f0efcd850644b42d8ca942430f7ee8d
Muminul Islam <muislam@microsoft.com>
2025-10-14 17:03:37 +0000

    hyperv: uapi: update partition property IOCTL definition
    The current IOCTL does not allow to write the property
    value back to user space. This patch fixes the IOCTL definition
    and corresponding input/output struct for the IOCTL.
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- include/uapi/linux/mshv.h
+++ include/uapi/linux/mshv.h
@@ -56,10 +56,6 @@ struct mshv_assert_interrupt {
 	__u32 rsvd;
 };
 
-struct mshv_partition_property {
-	enum hv_partition_property_code property_code;
-	__u64 property_value;
-};
 
 struct mshv_translate_gva {
 	__u64 gva;
@@ -216,9 +212,21 @@ struct mshv_create_partition_v2 {
 #endif
 } __packed;
 
+/**
+ * struct mshv_partition_property: arg for get/set_partition_property
+ * @property_code: code of the property
+ * @property_value: value of the property
+ * 
+ * Returns: same as the struct
+ */
+struct mshv_partition_property {
+	__u64 property_code;
+	__u64 property_value;
+};
+
 /* /dev/mshv */
 #define MSHV_CREATE_PARTITION	_IOW(MSHV_IOCTL, 0x00, struct mshv_create_partition)
-#define MSHV_GET_HOST_PARTITION_PROPERTY _IOR(MSHV_IOCTL, 0x01, u64)
+#define MSHV_GET_HOST_PARTITION_PROPERTY _IOWR(MSHV_IOCTL, 0x01, struct mshv_partition_property)
 /* Start nr again from 0x00 - mshv_vtl ioctls won't collide with mshv_root */
 #define MSHV_CREATE_VTL		_IO(MSHV_IOCTL, 0x00)
 #define MSHV_GET_VTL_CAPS	_IOR(MSHV_IOCTL, 0x01, struct mshv_vtl_capabilities)
