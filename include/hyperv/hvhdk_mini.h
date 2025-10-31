d7b897b1a8a9ff928b19623ed66df4b1aa9807aa
Andreea Pintilie <anpintil@microsoft.com>
2025-10-31 20:08:12 +0000

    hyperv: sync guest capability flags with Hyper-V
    Problem:
    Some Hyper-V capability bit definitions in the guest were out of date
    and did not match what the host expects. This could cause incorrect
    feature reporting.
    
    Solution:
    Update the capability to match the ones in the hypervisor.
    
    Signed-off-by: Andreea Pintilie <anpintil@microsoft.com>
    
    (cherry picked from commit d128e26f2965590efdb7f62c1039c6a491f4291b)

--- include/hyperv/hvhdk_mini.h
+++ include/hyperv/hvhdk_mini.h
@@ -173,7 +173,8 @@ struct hv_partition_property_vmm_capabilities {
 #else
 			u64 reservedbit3 : 1;
 #endif
-			u64 assignable_synthetic_proc_features : 1;
+			u64 assignable_synthetic_proc_features: 1;
+			u64 tag_hv_message_from_child: 1;
 			u64 vmm_enable_integrated_scheduler : 1;
 			u64 reserved0 : HV_PARTITION_VMM_CAPABILITIES_RESERVED_BITFIELD_COUNT;
 		} __packed;
