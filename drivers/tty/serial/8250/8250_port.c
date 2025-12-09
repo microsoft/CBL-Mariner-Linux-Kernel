ff9e9a5a88034ebf4535612267102d5a479c7e53
Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>
2025-12-09 18:45:38 -0500

    Revert "serial: 8250: Adjust the timeout for FIFO mode"
    This reverts commit d91f98be26510f5f81ec66425bb0306d1ccd571a.
    
    Signed-off-by: Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>

--- drivers/tty/serial/8250/8250_port.c
+++ drivers/tty/serial/8250/8250_port.c
@@ -1943,8 +1943,7 @@ static void serial8250_break_ctl(struct uart_port *port, int break_state)
 	serial_port_out(port, UART_LCR, up->lcr);
 }
 
-/* Returns true if @bits were set, false on timeout */
-static bool wait_for_lsr(struct uart_8250_port *up, int bits)
+static void wait_for_lsr(struct uart_8250_port *up, int bits)
 {
 	unsigned int status, tmout;
 
@@ -1967,11 +1966,11 @@ static bool wait_for_lsr(struct uart_8250_port *up, int bits)
 		udelay(1);
 		touch_nmi_watchdog();
 	}
-
-	return (tmout != 0);
 }
 
-/* Wait for transmitter and holding register to empty with timeout */
+/*
+ *	Wait for transmitter & holding register to empty
+ */
 static void wait_for_xmitr(struct uart_8250_port *up, int bits)
 {
 	unsigned int tmout;
@@ -3226,16 +3225,6 @@ static void serial8250_console_restore(struct uart_8250_port *up)
 	serial8250_out_MCR(up, up->mcr | UART_MCR_DTR | UART_MCR_RTS);
 }
 
-static void fifo_wait_for_lsr(struct uart_8250_port *up, unsigned int count)
-{
-	unsigned int i;
-
-	for (i = 0; i < count; i++) {
-		if (wait_for_lsr(up, UART_LSR_THRE))
-			return;
-	}
-}
-
 /*
  * Print a string to the serial port using the device FIFO
  *
@@ -3245,16 +3234,14 @@ static void fifo_wait_for_lsr(struct uart_8250_port *up, unsigned int count)
 static void serial8250_console_fifo_write(struct uart_8250_port *up,
 					  const char *s, unsigned int count)
 {
+	int i;
 	const char *end = s + count;
 	unsigned int fifosize = up->tx_loadsz;
 	struct uart_port *port = &up->port;
-	unsigned int tx_count = 0;
 	bool cr_sent = false;
-	unsigned int i;
 
 	while (s != end) {
-		/* Allow timeout for each byte of a possibly full FIFO */
-		fifo_wait_for_lsr(up, fifosize);
+		wait_for_lsr(up, UART_LSR_THRE);
 
 		for (i = 0; i < fifosize && s != end; ++i) {
 			if (*s == '\n' && !cr_sent) {
@@ -3265,14 +3252,7 @@ static void serial8250_console_fifo_write(struct uart_8250_port *up,
 				cr_sent = false;
 			}
 		}
-		tx_count = i;
 	}
-
-	/*
-	 * Allow timeout for each byte written since the caller will only wait
-	 * for UART_LSR_BOTH_EMPTY using the timeout of a single character
-	 */
-	fifo_wait_for_lsr(up, tx_count);
 }
 
 /*
