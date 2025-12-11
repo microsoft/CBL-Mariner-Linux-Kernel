9ced5005ef060ca8eb183e4fc26a1ba2e4201888
Florian Westphal <fw@strlen.de>
2025-12-11 12:55:19 +0100

    netfilter: nf_tables: avoid softlockup warnings in nft_chain_validate
    This reverts commit
    314c82841602 ("netfilter: nf_tables: can't schedule in nft_chain_validate"):
    Since commit a60a5abe19d6 ("netfilter: nf_tables: allow iter callbacks to sleep")
    the iterator callback is invoked without rcu read lock held, so this
    cond_resched() is now valid.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    (cherry picked from commit 7e7a817f2dfd79098a706ee5581ea9518b2de878)
    Signed-off-by: Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -3888,6 +3888,8 @@ int nft_chain_validate(const struct nft_ctx *ctx, struct nft_chain *chain)
 			if (err < 0)
 				return err;
 		}
+
+		cond_resched();
 	}
 
 	nft_chain_vstate_update(ctx, chain);
@@ -3912,8 +3914,6 @@ static int nft_table_validate(struct net *net, const struct nft_table *table)
 		err = nft_chain_validate(&ctx, chain);
 		if (err < 0)
 			goto err;
-
-		cond_resched();
 	}
 
 err:
@@ -11016,6 +11016,8 @@ static int nft_validate_register_store(const struct nft_ctx *ctx,
 
 		return 0;
 	}
+
+	return 0;
 }
 
 int nft_parse_register_store(const struct nft_ctx *ctx,
