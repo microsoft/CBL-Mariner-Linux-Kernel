4fb2c6f391b287f82fa29d046774ef5a4cfcb594
Rachel Menge <rachelmenge@microsoft.com>
2025-12-17 22:37:25 -0800

    Remove name from logging since unavailable at time of signature verification

--- kernel/module/signing.c
+++ kernel/module/signing.c
@@ -71,7 +71,7 @@ int mod_verify_sig(const void *mod, struct load_info *info)
 				VERIFYING_MODULE_SIGNATURE,
 				NULL, NULL);
 		if (!ret)
-			pr_warn("OOT module '%s' signed by key in platform keyring verified.\n", info->name);
+			pr_warn("OOT module signed by key in platform keyring verified.\n");
 	}
 	return ret;
 }
