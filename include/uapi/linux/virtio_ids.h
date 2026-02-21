126a9c5724d51aa204c14164914e4cb6179944ae
Cameron Baird <cameronbaird@microsoft.com>
2026-02-21 01:43:41 +0000

    Correct virtio_id (use upstream consensus rather than placeholder 17)

--- include/uapi/linux/virtio_ids.h
+++ include/uapi/linux/virtio_ids.h
@@ -43,7 +43,7 @@
 #define VIRTIO_ID_CAIF			12 /* Virtio caif */
 #define VIRTIO_ID_MEMORY_BALLOON	13 /* virtio memory balloon */
 #define VIRTIO_ID_GPU			16 /* virtio GPU */
-#define VIRTIO_ID_CLOCK			17 /* virtio clock/timer */
+#define VIRTIO_ID_CLOCK			44 /* virtio clock/timer */
 #define VIRTIO_ID_INPUT			18 /* virtio input */
 #define VIRTIO_ID_VSOCK			19 /* virtio vsock transport */
 #define VIRTIO_ID_CRYPTO		20 /* virtio crypto */
