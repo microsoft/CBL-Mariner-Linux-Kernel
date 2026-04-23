04142716552cec26d46c5b4a2eb9d322ba2ea14d
Jeff Barnes <jeffbarnes@linux.microsoft.com>
2026-04-23 11:21:41 -0400

    crypto: testmgr - disallow RSA PKCS#1 SHA-1 sig algs in FIPS mode
    When booted with fips=1, RSA signature generation using SHA-1 must not be
    available.  However, pkcs1pad(rsa,sha1) can currently be instantiated
    because it is not present in alg_test_descs; alg_test() falls through the
    no_test path and succeeds, after which the algorithm appears in
    /proc/crypto as fips-capable.
    
    Add explicit alg_test_descs entries for pkcs1pad(rsa,sha1) and
    pkcs1(rsa,sha1) without marking them fips_allowed, so they are treated as
    not FIPS-allowed when fips=1 is enabled.
    
    Include both names to cover kernels where RSA sign/verify is provided via
    the pkcs1(...) signature template, while pkcs1pad(...) remains for the
    traditional wrapper naming and/or RSAES operations.
    
    Signed-off-by: Jeff Barnes <jeffbarnes@linux.microsoft.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    (cherry picked from commit 76d2e560c76dabbf9f31f4311069e68610d1ac18)

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -5333,6 +5333,9 @@ static const struct alg_test_desc alg_test_descs[] = {
 			.sig = __VECS(pkcs1_rsa_none_tv_template)
 		}
 	}, {
+		.alg = "pkcs1(rsa,sha1)",
+		.test = alg_test_null,
+	}, {
 		.alg = "pkcs1(rsa,sha224)",
 		.test = alg_test_null,
 		.fips_allowed = 1,
@@ -5367,6 +5370,9 @@ static const struct alg_test_desc alg_test_descs[] = {
 		.alg = "pkcs1pad(rsa)",
 		.test = alg_test_null,
 		.fips_allowed = 1,
+	}, {
+		.alg = "pkcs1pad(rsa,sha1)",
+		.test = alg_test_null,
 	}, {
 		.alg = "polyval",
 		.test = alg_test_hash,
