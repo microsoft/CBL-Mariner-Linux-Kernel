e7f7873c45ec91dc4198f5162dd1d6c47a64cbc4
Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>
2026-08-06 15:38:34 -0400

    Revert "PCI: Fix restoring BARs on BAR resize rollback path"
    This reverts commit 0b52ed5ec3debaaab8c653b65ebcc0ec6a58eae8.
    
    Signed-off-by: Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>

--- drivers/pci/setup-res.c
+++ drivers/pci/setup-res.c
@@ -427,9 +427,9 @@ void pci_release_resource(struct pci_dev *dev, int resno)
 }
 EXPORT_SYMBOL(pci_release_resource);
 
-int pci_resize_resource(struct pci_dev *dev, int resno, int size,
-			int exclude_bars)
+int pci_resize_resource(struct pci_dev *dev, int resno, int size)
 {
+	struct resource *res = dev->resource + resno;
 	struct pci_host_bridge *host;
 	int old, ret;
 	u32 sizes;
@@ -439,6 +439,10 @@ int pci_resize_resource(struct pci_dev *dev, int resno, int size,
 	host = pci_find_host_bridge(dev->bus);
 	if (host->preserve_config)
 		return -ENOTSUPP;
+
+	/* Make sure the resource isn't assigned before resizing it. */
+	if (!(res->flags & IORESOURCE_UNSET))
+		return -EBUSY;
 
 	pci_read_config_word(dev, PCI_COMMAND, &cmd);
 	if (cmd & PCI_COMMAND_MEMORY)
@@ -459,14 +463,19 @@ int pci_resize_resource(struct pci_dev *dev, int resno, int size,
 	if (ret)
 		return ret;
 
-	ret = pci_do_resource_release_and_resize(dev, resno, size, exclude_bars);
-	if (ret)
-		goto error_resize;
+	res->end = res->start + pci_rebar_size_to_bytes(size) - 1;
 
+	/* Check if the new config works by trying to assign everything. */
+	if (dev->bus->self) {
+		ret = pci_reassign_bridge_resources(dev->bus->self, res->flags);
+		if (ret)
+			goto error_resize;
+	}
 	return 0;
 
 error_resize:
 	pci_rebar_set_size(dev, resno, old);
+	res->end = res->start + pci_rebar_size_to_bytes(old) - 1;
 	return ret;
 }
 EXPORT_SYMBOL(pci_resize_resource);
