4490bf0d9cf96636b1f735f76fa8b5232028f702
Siddharth Chintamaneni <siddharthc@microsoft.com>
2026-09-03 17:40:04 +0000

    Revert "xsk: validate launch-time metadata size"
    This reverts commit af511afa1d2977f384044df78d6fbf9fba653f7a.

--- include/net/xdp_sock_drv.h
+++ include/net/xdp_sock_drv.h
@@ -256,28 +256,21 @@ xsk_buff_raw_get_ctx(const struct xsk_buff_pool *pool, u64 addr)
 	0)
 
 static inline bool
-xsk_buff_valid_tx_metadata(const struct xsk_buff_pool *pool,
-			   const struct xsk_tx_metadata *meta, u64 *flags)
+xsk_buff_valid_tx_metadata(const struct xsk_tx_metadata *meta)
 {
-	*flags = READ_ONCE(meta->flags);
-	if (*flags & XDP_TXMD_FLAGS_LAUNCH_TIME)
-		if (pool->tx_metadata_len <
-		    offsetofend(struct xsk_tx_metadata, request.launch_time))
-			return false;
-	return !(*flags & ~XDP_TXMD_FLAGS_VALID);
+	return !(meta->flags & ~XDP_TXMD_FLAGS_VALID);
 }
 
 static inline struct xsk_tx_metadata *
 __xsk_buff_get_metadata(const struct xsk_buff_pool *pool, void *data)
 {
 	struct xsk_tx_metadata *meta;
-	u64 flags;
 
 	if (!pool->tx_metadata_len)
 		return NULL;
 
 	meta = data - pool->tx_metadata_len;
-	if (unlikely(!xsk_buff_valid_tx_metadata(pool, meta, &flags)))
+	if (unlikely(!xsk_buff_valid_tx_metadata(meta)))
 		return NULL; /* no way to signal the error to the user */
 
 	return meta;
@@ -472,9 +465,7 @@ xsk_buff_raw_get_ctx(const struct xsk_buff_pool *pool, u64 addr)
 	return (struct xdp_desc_ctx){ };
 }
 
-static inline bool
-xsk_buff_valid_tx_metadata(const struct xsk_buff_pool *pool,
-			   const struct xsk_tx_metadata *meta, u64 *flags)
+static inline bool xsk_buff_valid_tx_metadata(struct xsk_tx_metadata *meta)
 {
 	return false;
 }
