4852ac28c26eb2a18e2699427d4809175724e3de
Siddharth Chintamaneni <siddharthc@microsoft.com>
2026-09-03 17:40:04 +0000

    Revert "xsk: pass TX metadata pointer by reference"
    This reverts commit 5ec4f525373bc58e197e05f97a92d03a845a92f7.

--- include/net/xdp_sock.h
+++ include/net/xdp_sock.h
@@ -152,19 +152,17 @@ static inline void xsk_tx_metadata_to_compl(struct xsk_tx_metadata *meta,
 /**
  *  xsk_tx_metadata_request - Evaluate AF_XDP TX metadata at submission
  *  and call appropriate xsk_tx_metadata_ops operation.
- *  @pmeta: pointer to pointer to AF_XDP metadata area
+ *  @meta: pointer to AF_XDP metadata area
  *  @ops: pointer to struct xsk_tx_metadata_ops
  *  @priv: pointer to driver-private aread
  *
  *  This function should be called by the networking device when
  *  it prepares AF_XDP egress packet.
  */
-static inline void xsk_tx_metadata_request(struct xsk_tx_metadata **pmeta,
+static inline void xsk_tx_metadata_request(const struct xsk_tx_metadata *meta,
 					   const struct xsk_tx_metadata_ops *ops,
 					   void *priv)
 {
-	const struct xsk_tx_metadata *meta = *pmeta;
-
 	if (!meta)
 		return;
 
@@ -226,7 +224,7 @@ static inline void xsk_tx_metadata_to_compl(struct xsk_tx_metadata *meta,
 {
 }
 
-static inline void xsk_tx_metadata_request(struct xsk_tx_metadata **pmeta,
+static inline void xsk_tx_metadata_request(struct xsk_tx_metadata *meta,
 					   const struct xsk_tx_metadata_ops *ops,
 					   void *priv)
 {
